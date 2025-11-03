fc008b4d04e87c11f36997ad4c0a533a2a0bc934
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2025-11-03 11:08:58 -0700

    added function show image using api

--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -1,19 +1,46 @@
-import ImageCard from "./ImageCard";
+import { useEffect, useState } from "react";
+
+import ImageCard from "./ImageCard.jsx"; 
+
 
 export default function ImageGrid() {
-    const placeholders = [
-        "https://source.unsplash.com/random/800x600?nature",
-        "https://source.unsplash.com/random/800x600?technogy",
-        "https://source.unsplash.com/random/800x600?space",
-        "https://source.unsplash.com/random/800x600?architecture",
-        "https://source.unsplash.com/random/800x600?travel",
-    ];
+    const [photos, setPhotos] = useState([]);
 
+    const fetchPhotos = async () => {
+        try {
+         const response = await fetch("https://api.unsplash.com/search/photos?query=space&per_page=5&page=1",  {
+
+        headers: {
+            Authorization: "Client-ID YOUR_ACCESS_KEY ", //require authorization  // YOUR_ACCESS_KEY 
+          },
+        }
+    );
+        if (!response.ok) {
+            throw new Error(`Error ${response.status}`);
+        }
+        
+        const data = await response.json();
+
+        // new photos to previous one
+        setPhotos(data.results);
+
+        console.log("Fetched photos:", data.results);
+    } catch (err) {
+        console.error("Fetch failed:", err);
+      }
+    };
+
+    useEffect(() => {
+        fetchPhotos();
+    }, []);
+
+   
     return (
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            {placeholders.map((src, i) => (
-                <ImageCard key={i} img={src} />
-            ))}
+        {photos.map((photo) => (
+          <ImageCard key={photo.id} img={photo.urls.small} />
+          ))}
         </div>
+  
     );
-}
+  }
